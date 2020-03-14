4bd946e05141b686520abdf39bc966d69aba1d95
Eduardo Castillo <ecastillo@anzen.com.mx>
2020-03-14 12:43:44 -0600

    first commit

---
+++ app/src/main/java/com/certificatic/mobilebanking/common/Constants.java
@@ -0,0 +1,6 @@
+package com.certificatic.mobilebanking.common;
+
+public class Constants {
+    public static final String API_LOGIN_BASE_URL="https://logincurso.herokuapp.com";
+    public static final String API_CLIENT_URL="https://api-webinar.herokuapp.com";
+}
